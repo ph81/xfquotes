a13c375848be693a5e19a2c460460b5c020e8cef
Edith M <edith.mejia@gmail.com>
2021-09-16 20:57:24 -0500

    Styles updated

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const image = document.querySelector('.char-image');
+
+const getRandomQuote = () => {
+  const random = Math.floor(Math.random() * quotes.length);
+  return quotes[random];
+};
+
+const displayQuote = () => {
+  let quote = getRandomQuote();
+  let quoteHtml = '';
+  let authorHtml = '';
+
+  quoteHtml += `"${quote.quote}"`;
+  //authorHtml += `— ${quote.author}`; - replacing to test XF quotes
+  authorHtml += `<strong>${quote.character}</strong> &nbsp; (${quote.episode} - Season ${quote.season})`;
+
+
+  document.querySelector('.quote-text').innerHTML = quoteHtml;
+  document.querySelector('.quote-author').innerHTML = authorHtml;
+};
+
+//displayQuote();
+
+image.addEventListener('click', () => {
+  displayQuote();
+});
---
+++ js/xfquotes.js
@@ -0,0 +1,182 @@
+const quotes = [
+	{
+        quote: "Sorry, nobody down here but the FBI's most unwanted.",
+        character: "Fox Mulder",
+        episode: "Pilot",
+        season: 1
+    },
+    {
+        quote: "Agent Mulder believes we are not alone.",
+        character: "Dana Scully",
+        episode: "Pilot",
+        season: 1
+    },
+    {
+        quote: "Let's just say this case has a... distinct smell to it. A certain... paranormal bouquet.",
+        character: "Fox Mulder",
+        episode: "Deep Throat",
+        season: 1
+    },
+    {
+        quote: "Mister Mulder, they've been here for a long, long time.",
+        character: "Deep Throat",
+        episode: "Deep Throat",
+        season: 1
+    },
+    {
+        quote: "Do you think I'm spooky?",
+        character: "Fox Mulder",
+        episode: "Squeeze",
+        season: 1
+    },
+    {
+        quote: "You know when I was a kid, I had this ritual. I closed my eyes before I walked into my room, 'cause I thought that one day when I opened them my sister would be there. Just lying in bed, like nothing ever happened. You know I'm still walking into that room, everyday of my life.",
+        character: "Fox Mulder",
+        episode: "Conduit",
+        season: 1
+    },
+          {
+        quote: "I want to believe.",
+        character: "Fox Mulder",
+        episode: "Conduit",
+        season: 1
+      },
+          {
+        quote: "Keep it up, Mulder, and I'll hurt you like that beast woman.",
+        character: "Dana Scully",
+        episode: "The Jersey Devil",
+        season: 1
+      },
+          {
+        quote: "You can deny all the things I've seen, all the things I've discovered, but not for much longer because too many others know what's happening out there. And no one, no government agency, has jurisdiction over the truth.",
+        character: "Fox Mulder",
+        episode: "Fallen Angel",
+        season: 1
+      },
+      {
+        quote: "Well, that's one of the luxuries of hunting down aliens and genetic mutants. You rarely get to press charges.",
+        character: "Fox Mulder",
+        episode: "Fire",
+        season: 1
+      },
+          {
+        quote: "I'm afraid. I'm afraid to believe.",
+        character: "Dana Scully",
+        episode: "Beyond The Sea",
+        season: 1
+      },
+          {
+        quote: "That's why we like you, Mulder. Your ideas are weirder than ours.",
+        character: "Byers",
+        episode: "E.B.E.",
+        season: 1
+      },
+          {
+        quote: "I was raised a Catholic. I have a certain... familiarity with the scripture. And God never lets the Devil steal the show.",
+        character: "Dana Scully",
+        episode: "Miracle Man",
+        season: 1
+      },
+          {
+        quote: "If there's an ice tea in that bag, could be love.",
+        character: "Fox Mulder",
+        episode: "Tooms",
+        season: 1
+      },
+          {
+        quote: "Mulder, I wouldn't put myself on the line for anybody but you.",
+        character: "Dana Scully",
+        episode: "Tooms",
+        season: 1
+      },
+          {
+        quote: "Okay, Mulder. But I'm warning you, if this is monkey pee, you're on your own.",
+        character: "Dana Scully",
+        episode: "The Erlenmeyer Flask",
+        season: 2
+      },
+          {
+        quote: "Trust.. no one.",
+        character: "Deep Throat",
+        episode: "The Erlenmeyer Flask",
+        season: 1
+      },
+          {
+        quote: "I may not have the X-Files, Scully, but I still have my work. And I've still got you. And I still have myself.",
+        character: "Fox Mulder",
+        episode: "Little Green Men",
+        season: 2
+      },
+          {
+        quote: "Kill Mulder and you risk turning one man's religion into a crusade.",
+        character: "CSM",
+        episode: "Ascension",
+        season: 2
+      },
+      {
+        quote: "There are no answers for you, Mr. Mulder. They only have one policy... Deny everything.",
+        character: "Mr. X",
+        episode: "Ascension",
+        season: 2
+      },
+          {
+        quote: "I never knew how much I loved my daughter until I could never tell her... At that moment I would have traded every medal, every commendation, every promotion for just one more second with you... We'll be together again, Starbuck. Not now. Soon.",
+        character: "Bill Scully",
+        episode: "One Breath",
+        season: 2
+      },
+          {
+        quote: "Not Fox, Mulder.",
+        character: "Dana Scully",
+        episode: "One Breath",
+        season: 2
+      },
+      {
+        quote: " I've always figured that dreams are answers to questions we haven't figured out how to ask yet.",
+        character: "Fox Mulder",
+        episode: "Aubrey",
+        season: 2
+      },
+      {
+        quote: "You think you find a way to deal with these things. In med school, you develop a clinical detachment to death. In your FBI training, you are confronted with cases, the most terrible and violent cases. You think you can look into the face of pure evil. And then you find yourself paralyzed by it.",
+        character: "Dana Scully",
+        episode: "Irresistible",
+        season: 2
+      },
+      {
+        quote: "We predict the future. And the best way to predict it, is to invent it.",
+        character: "Well-Manicured Man",
+        episode: "The Blessing Way",
+        season: 3
+      },
+      {
+        quote: " I'm afraid that God is speaking and no one is listening.",
+        character: "Dana Scully",
+        episode: "Revelations",
+        season: 3
+      },
+      {
+        quote: "Her name is Bambi?",
+        character: "Dana Scully",
+        episode: "The War of the Coprophages",
+        season: 3
+      },
+      {
+        quote: "Sure, fine, whatever.",
+        character: "Dana Scully",
+        episode: "Sysygy",
+        season: 3
+      },
+      {
+        quote: "Nothing happens in contradiction to nature, only in contradiction to what we know of it. And that's a place to start. That's where the hope is.",
+        character: "Dana Scully",
+        episode: "Herrenvolk",
+        season: 4
+      },
+      {
+        quote: "You can't ask the truth of a man who trades in lies.",
+        character: "Walter Skinner",
+        episode: "Memento Mori",
+        season: 4
+      }
+];
